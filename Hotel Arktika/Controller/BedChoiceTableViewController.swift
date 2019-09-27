95eadb95d9689cc782887d6152ba2bbbbfd56316
Aleksey Popov <aplexus11@yandex.ru>
2019-09-27 14:25:13 +0300

    Add second tableView

---
+++ Hotel Arktika/Controller/BedChoiceTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BedChoiceTableViewController.swift
+//  Hotel Arktika
+//
+//  Created by Aleksey Popov on 27/09/2019.
+//  Copyright © 2019 Aleksey Popov. All rights reserved.
+//
+
+import UIKit
+
+class BedChoiceTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
